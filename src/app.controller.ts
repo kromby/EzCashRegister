fb55111a216c385f980dccbac39adf0325b63e8f
Guðjón Karl Arnarson <kromby@hress.org>
2023-07-12 14:23:31 +0000

    Adding HTML tags

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,9 +17,16 @@ export class AppController {
     const path = './src/assets/input.txt';
     const input = fs.readFileSync(path, 'utf-8');
     this.loggerService.log('Input: ' + input, 'AppController');
-    const output = this.keepthechangeService
-      .calculateChange(input, 3)
-      .replaceAll('\n', '<br/>');
-    return output;
+    const output = this.keepthechangeService.calculateChange(input, 3);
+
+    this.loggerService.log('Output: \n' + output, 'AppController');
+
+    return (
+      '<html><body><h1>Input</h1>' +
+      input +
+      '<h1>Output</h1>' +
+      output.replaceAll('\n', '<br/>') +
+      '</body></html>'
+    );
   }
 }
